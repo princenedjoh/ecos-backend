6fc71877378f0d2e4b683267b2fd8c28d70d3efe
princenedjoh <62966723+princenedjoh@users.noreply.github.com>
2024-06-24 05:02:49 +0000

    changed access token lifetime

--- djangoBackend/settings.py
+++ djangoBackend/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 ]
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
 }
 
